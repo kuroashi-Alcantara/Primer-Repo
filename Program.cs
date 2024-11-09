002e2eaba8a12451f32c0f4949a8300f96373fe2
Felix <mrprincevinsmoke50@gmail.com>
2024-11-08 20:28:03 -0400

    agregando excepciones

--- Program.cs
+++ Program.cs
@@ -1,28 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 double result = 0;
+string prove;
+int num1 = 0;
+int num2 = 0;
 
-Console.WriteLine("ngresa un numero"); int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Ingresa otro numero"); int num2 = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Desea: \nsumar: +\nrestar: -\nmultiplicar: *\ndividir: /"); string op = Console.ReadLine();
+try
+{
+Console.WriteLine("ingresa un numero");
+prove = Console.ReadLine();
+if (prove != null)
+{
+        num1 = int.Parse(prove);
+}
 
-switch(op)
+Console.WriteLine("Ingresa otro numero");
+prove = Console.ReadLine();
+if (prove != null)
+{    
+    num2 = int.Parse(prove);
+}
+
+
+System.Console.WriteLine("Desea: \nsumar: +\nrestar: -\nmultiplicar: *\ndividir: /");
+
+string op = Console.ReadLine();
+
+switch (op)
 {
     case "+":
-    result = num1 +num2;
-    break;
+        result = num1 + num2;
+        break;
 
     case "-":
-    result = num1-num2;
-    break;
+        result = num1 - num2;
+        break;
 
     case "*":
-    result = num1*num2;
-    break;
+        result = num1 * num2;
+        break;
 
     case "/":
-    result =(double)num1 / num2;
-    break;
+        result = (double)num1 / num2;
+        break;
 }
 
 System.Console.WriteLine($"resultado {result}");
+
+}
+catch(Exception ex)
+{
+    System.Console.WriteLine(ex.Message);
+}
